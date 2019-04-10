a18210b84d6aae9c7d4a5b5993d80051dd260bc9
sadainiqrar <sadain.abbasi@gmail.com>
2019-04-10 23:41:11 +0500

    allow cors

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-var express = require('express');
-var compress = require('compression');
+var express = require("express");
+var compress = require("compression");
 var app = express();
 
 // Use compress middleware to gzip content
 app.use(compress());
 
-// Serve up content from public directory
-app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-app.listen(process.env.PORT || 9000);
+// Serve up content from public directory
+app.use(express.static(__dirname + "/public"));
+
+app.listen(9000);
